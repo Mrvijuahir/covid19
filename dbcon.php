ae821d2e8915d6cd7b766d1ee73c350ad36acc31
Vijay Ahir <vahir9487@gmail.com>
2021-04-11 18:12:33 +0530

    commit message

--- dbcon.php
+++ dbcon.php
@@ -1,9 +1,9 @@
 <?php
 
-$server='localhost';
-$username='root';
-$password='';
-$db='coronadb';
+$server='remotemysql.com';
+$username='YoAXC5DS0b';
+$password='rH3aiL8I7M';
+$db='YoAXC5DS0b';
 
 $con=mysqli_connect($server,$username,$password,$db);
 if($con){
